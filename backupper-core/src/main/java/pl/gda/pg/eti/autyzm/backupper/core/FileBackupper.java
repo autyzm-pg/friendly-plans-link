90c23173db00e4f0150325c00953dbcc8fe45c15
Illia Ovchynnikov <illia.ovchynnikov@gmail.com>
2016-06-22 20:13:30 +0200

    Add dump template for Backupper implementation

---
+++ backupper-core/src/main/java/pl/gda/pg/eti/autyzm/backupper/core/FileBackupper.java
@@ -0,0 +1,27 @@
+package pl.gda.pg.eti.autyzm.backupper.core;
+
+import pl.gda.pg.eti.autyzm.backupper.api.Backup;
+import pl.gda.pg.eti.autyzm.backupper.api.Backupper;
+import pl.gda.pg.eti.autyzm.backupper.api.BackupperException;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Created by superuser on 22-Jun-16.
+ */
+public class FileBackupper implements Backupper {
+    @Override public void makeBackup(String backupName, URI pathToDevice)
+        throws BackupperException {
+        
+    }
+
+    @Override public List<Backup> getBackups() throws BackupperException {
+        return null;
+    }
+
+    @Override public Optional<Backup> getBackup(String backupName) throws BackupperException {
+        return null;
+    }
+}
